8731011563fbec640e2f8ff35de8a98b68e615eb
venus718 <venusdragon718@gmail.com>
2015-04-27 10:20:28 +0300

    added redirections

--- aftersale.php
+++ aftersale.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * afterlead.php
+ * aftersale.php
  * User: sami
- * Date: 26-Mar-15
- * Time: 9:28 PM
+ * Date: 27-Apr-15
+ * Time: 10:19 AM
  */
-require __DIR__ . '/header.php';
 
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -18,8 +17,3 @@ if('yes' !== $visited) {
     $response = new RedirectResponse('terms.php');
     $response->send();
 }
-
-/** @var \controller\LeadController $leadController */
-$leadController = $container->get('controller.lead');
-$theLead = $leadController->getLeadAction($leadController->getLastLeadId());
-dump($theLead);
